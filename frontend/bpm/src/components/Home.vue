6afffaccfa2138e41ac61a7f91fe4a491b7906aa
Brandon <brandu.tan@gmail.com>
2021-04-25 03:33:58 +0800

    Cleaned up dummy data

--- frontend/bpm/src/components/Home.vue
+++ frontend/bpm/src/components/Home.vue
@@ -71,13 +71,11 @@ export default {
       api_secret: "",
       dummy_asset: [
         { Coin: "ALGO", Quantity: 256.46999999999997, Price: "1.25550000" },
-        { Coin: "ATOM", Quantity: 0.03899999999999437, Price: "20.45900000" },
         { Coin: "BNB", Quantity: 0.772, Price: "546.90940000" },
         { Coin: "BTC", Quantity: 0.0112718, Price: "53375.84000000" },
         { Coin: "DOT", Quantity: 6.23, Price: "34.93220000" },
         { Coin: "EGLD", Quantity: 1.589, Price: "175.50000000" },
         { Coin: "ETH", Quantity: 0.20609000000000005, Price: "2553.28000000" },
-        { Coin: "NEAR", Quantity: 0.10999999999999943, Price: "5.15720000" },
         { Coin: "USDT", Quantity: 1.0215684, Price: 1 },
         { Coin: "ZIL", Quantity: 1371.4, Price: "0.18100000" },
       ],
