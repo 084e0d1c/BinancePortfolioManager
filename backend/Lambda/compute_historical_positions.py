ab72188609241d69030e9f62e2a8b688a9db8391
Brandon <brandu.tan@gmail.com>
2021-04-24 22:07:23 +0800

    Updated to convert timestamp to unix time for serialisation purposes

--- backend/Lambda/compute_historical_positions.py
+++ backend/Lambda/compute_historical_positions.py
@@ -1,14 +1,8 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from os import environ
+import json
 import pandas as pd
 from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 import numpy as np
 from time import mktime
-
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
 
 def convert_to_datetime(t):
     dt = datetime.fromtimestamp(t/1000.0).strftime("%Y-%m-%d %H:%M:%S")
@@ -30,9 +24,9 @@ def clean_name(coin_name):
         clean_coin_name = clean_coin_name.split(c)[0]
     return clean_coin_name
 
-@app.route('/compute_historical_positions',methods=['POST'])
-def compute_position():
-    order_history = request.get_json()['data']
+def compute_position(event, context):
+
+    order_history = json.loads(event['body'])
     order_df = pd.DataFrame(order_history)
     order_df = order_df.apply(pd.to_numeric, errors='ignore')
     order_df = order_df[order_df['executedQty'] > 0]
@@ -45,7 +39,7 @@ def compute_position():
     order_df['side'] = np.where(order_df['side']=="BUY",1,-1)
 
     order_df['executedQty'] = order_df['executedQty'] * order_df['side']
-    price_df = pd.read_csv('../DevFiles/Price.csv')
+    price_df = pd.read_csv('../../DevFiles/Price.csv')
     price_df = price_df[list(order_df['symbol'].unique())+['timestamp']]
     price_df['timestamp'] = pd.to_datetime(price_df['timestamp'])
     price_df = price_df[price_df['timestamp'] >= order_df['updateTime'].min()-timedelta(hours=4)]
@@ -69,11 +63,12 @@ def compute_position():
         for col in nav_df.columns:
             nav_timeseries_data.append([unix_secs,col,nav_df.loc[t,col]])
 
-    return jsonify({
-        'code':200,
-        'data':nav_timeseries_data
-    }),200
+    response = {
+        "statusCode": "200",
+        "headers": {'Access-Control-Allow-Origin': "*"},
+        "body": json.dumps({
+            "data": nav_timeseries_data
+        })
+    }
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5003,debug=environ.get('PROD_ENV') != False)
+    return response
