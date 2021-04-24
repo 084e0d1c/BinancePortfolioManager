882df85365592119084234e9004ae6cf3c6f69ee
Brandon <brandu.tan@gmail.com>
2021-04-24 22:42:13 +0800

    Improved code neatness by using config to import endpoints

--- frontend/bpm/src/config.js
+++ frontend/bpm/src/config.js
@@ -1,1 +1,9 @@
-export const HOSTNAME = "http://localhost:";
+// Lambda Endpoints
+
+
+
+// Local DEV Endpoints
+const HOSTNAME = "http://localhost:";
+export const COMPUTE_WEIGHTS = HOSTNAME + "5001/compute_weights";
+export const COMPUTE_HISTORICAL_POS = HOSTNAME + "5003/compute_historical_positions";
+
