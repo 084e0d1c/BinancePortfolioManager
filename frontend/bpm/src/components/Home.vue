c6930192c370b9fad01e25968aa6b0e26d949646
Brandon <brandu.tan@gmail.com>
2021-04-26 00:32:32 +0800

    Reorg Dev Variables into CONSTANTs

--- frontend/bpm/src/components/Home.vue
+++ frontend/bpm/src/components/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <TopBar :key="componentKey" />
     <loading :active.sync="loading"></loading>
     <div class="container-fluid" style="margin-top: 25px">
       <div class="row justify-content-center">
-        <div class="col-sm-8 jumbotron" id="header" style="background: #42b983">
+        <div class="col-sm-6 jumbotron" id="header" style="background: #42b983">
           <h1 class="display-5" style="color: black">
-            Welcome to BPM!
+            Bianance Portfolio Manager
           </h1>
           <p class="lead" style="color: black">
             In no way or form is this website related to the official binance
@@ -16,34 +15,47 @@
         </div>
       </div>
     </div>
-    <div class="row justify-content-center" id="logIn">
-      <div class="col-sm-6">
-        <div class="form-group">
-          <label>API Secret </label>
-          <input
-            type="password"
-            class="form-control"
-            v-model="api_secret"
-            placeholder="API Secret"
-          />
+    <div class="container-fluid">
+      <div class="row justify-content-center" id="logIn">
+        <div class="col-sm-5">
+          <div class="form-group">
+            <label>API Secret </label>
+            <input
+              type="password"
+              class="form-control"
+              v-model="api_secret"
+              placeholder="API Secret"
+            />
+          </div>
+          <div class="form-group">
+            <label>API Key </label>
+            <input
+              type="password"
+              class="form-control"
+              v-model="api_key"
+              placeholder="API Password"
+            />
+          </div>
         </div>
-        <div class="form-group">
-          <label>API Key </label>
-          <input
-            type="password"
-            class="form-control"
-            v-model="api_key"
-            placeholder="API Password"
-          />
+      </div>
+      <br />
+      <div class="row justify-content-center">
+        <div class="col-sm-2">
+          <div class="text-center">
+            <button
+              class="btn btn-dark btn-block"
+              @click.prevent="handleSubmit()"
+            >
+              Let's Go!
+            </button>
+          </div>
         </div>
-        <br />
-        <div class="text-center">
-          <button
-            class="btn btn-dark btn-block"
-            @click.prevent="handleSubmit()"
-          >
-            Let's Go!
-          </button>
+        <div class="col-sm-2">
+          <div class="text-center">
+            <button class="btn btn-dark btn-block" @click.prevent="gotoDemo()">
+              Demo
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -51,35 +63,32 @@
 </template>
 
 <script>
-// import Loading from "vue-loading-overlay";
-import TopBar from "./TopBar";
-// Import component
 import Loading from "vue-loading-overlay";
-// Import stylesheet
 import "vue-loading-overlay/dist/vue-loading.css";
+import { DEV,DEMO_ASSETS, DEMO_ORDER_HISTORY } from "../config.js";
 
 export default {
   name: "Home",
   props: {},
   components: {
-    TopBar,
     Loading,
   },
   data() {
     return {
+      dev: DEV,
       api_key: "",
       api_secret: "",
-      dummy_asset: [
-        { Coin: "ALGO", Quantity: 256.46999999999997, Price: "1.25550000" },
-        { Coin: "BNB", Quantity: 0.772, Price: "546.90940000" },
-        { Coin: "BTC", Quantity: 0.0112718, Price: "53375.84000000" },
-        { Coin: "DOT", Quantity: 6.23, Price: "34.93220000" },
-        { Coin: "EGLD", Quantity: 1.589, Price: "175.50000000" },
-        { Coin: "ETH", Quantity: 0.20609000000000005, Price: "2553.28000000" },
-        { Coin: "USDT", Quantity: 1.0215684, Price: 1 },
-        { Coin: "ZIL", Quantity: 1371.4, Price: "0.18100000" },
-      ],
+      assets: [],
+      order_history: [],
     };
+  },
+  mounted() {
+    if (this.dev) {
+      this.assets = DEMO_ASSETS;
+      this.order_history = DEMO_ORDER_HISTORY;
+      this.api_secret = process.env.VUE_APP_API_SECRET;
+      this.api_key = process.env.VUE_APP_API_KEY;
+    }
   },
   methods: {
     handleSubmit() {
@@ -90,8 +99,19 @@ export default {
         API_KEY: this.api_key,
         API_SECRET: this.api_secret,
       });
-      this.$store.commit("change_assets", this.dummy_asset);
+      this.$store.commit("change_assets", this.assets);
+      this.$store.commit("change_order_history", this.order_history);
       this.$router.push("/portfolio");
+    },
+    gotoDemo() {
+      // To be updated later
+      this.loading = true;
+      this.assets = this.$store.demo_assets;
+      this.order_history = this.$store.demo_order_history;
+      this.api_key = "";
+      this.api_secret = "";
+      this.$store.commit("change_demo_status", true);
+      this.handleSubmit();
     },
   },
 };
