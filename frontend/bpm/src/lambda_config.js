92c01e01659c26632fe08fa2d07364a6758da4ab
Brandon <brandu.tan@gmail.com>
2021-05-02 18:56:43 +0800

    Added routes for new endpoints

--- frontend/bpm/src/lambda_config.js
+++ frontend/bpm/src/lambda_config.js
@@ -4,3 +4,5 @@ const APIGATEWAY_HOST =
 export const COMPUTE_WEIGHTS = APIGATEWAY_HOST + "/compute_weights";
 export const COMPUTE_HISTORICAL_POS = APIGATEWAY_HOST + "/compute_position";
 export const COMPUTE_PNL = APIGATEWAY_HOST + "/compute_pnl";
+export const GET_ALL_TRADES = APIGATEWAY_HOST + "/get_all_trades";
+export const GET_TICKERS = APIGATEWAY_HOST + "/get_valid_tickers";
