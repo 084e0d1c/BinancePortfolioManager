13b46cebe7c73592ebbde4677a710445dc97b9e8
Brandon <brandu.tan@gmail.com>
2021-05-02 18:56:22 +0800

    Reorganised layouts

--- frontend/bpm/src/components/Portfolio.vue
+++ frontend/bpm/src/components/Portfolio.vue
@@ -2,7 +2,38 @@
   <div>
     <NavBar :key="componentKey" />
     <loading :active.sync="loading"></loading>
-    <div>{{ api_key }} {{ api_key }}</div>
+    <div class="container" style="margin-top: 25px">
+      <div class="row justify-content-center">
+        <h3 class="display-10" style="color: black">
+          Portfolio at a Glance
+        </h3>
+      </div>
+      <div class="row justify-content-center" style="width:100%">
+        <b-card-group deck>
+          <b-card title="Total Portfolio Value">
+            <b-card-text> {{ portfolio_value }} (USD) </b-card-text>
+          </b-card>
+          <b-card title="Cumulative Total P&L">
+            <b-card-text> {{ total_pnl }} (USD) </b-card-text>
+          </b-card>
+          <b-card title="Highest Weight">
+            <b-card-text>
+              {{ coin_with_max_weight }}
+            </b-card-text>
+          </b-card>
+          <b-card title="Most Profitable">
+            <b-card-text>
+              {{ most_profitable }}
+            </b-card-text>
+          </b-card>
+          <b-card title="Least Profitable">
+            <b-card-text>
+              {{ least_profitable }}
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </div>
+    </div>
     <div class="container-fluid" style="margin-top: 25px">
       <div class="row justify-content-center">
         <h3 class="display-10" style="color: black">
@@ -30,7 +61,7 @@
         </h3>
       </div>
       <div class="row justify-content-center">
-        <div class="col-sm-6" id="header">
+        <div class="col-sm-12" id="header">
           <pnlChart
             type="stackedcolumn2d"
             width="100%"
@@ -40,16 +71,6 @@
           >
           </pnlChart>
         </div>
-        <div class="col-sm-6" id="header">
-          <pieChart
-            type="pie3d"
-            width="100%"
-            height="600"
-            dataFormat="json"
-            :dataSource="pieChartData"
-          >
-          </pieChart>
-        </div>
       </div>
       <div class="container-fluid" style="margin-top: 25px">
         <div class="row justify-content-center">
@@ -58,9 +79,19 @@
           </h3>
         </div>
         <div class="row justify-content-center">
-          <div class="col-sm-12" id="header">
+          <div class="col-sm-6" id="header">
             <b-table :items="assets" class="mt-2" outlined head-variant="dark">
             </b-table>
+          </div>
+          <div class="col-sm-6" id="header">
+            <pieChart
+              type="pie3d"
+              width="100%"
+              height="600"
+              dataFormat="json"
+              :dataSource="pieChartData"
+            >
+            </pieChart>
           </div>
         </div>
       </div>
@@ -72,11 +103,10 @@
 import NavBar from "./NavBar";
 import Loading from "vue-loading-overlay";
 import "vue-loading-overlay/dist/vue-loading.css";
-import { BTable } from "bootstrap-vue";
+import { BTable, BCard } from "bootstrap-vue";
 import Vue from "vue";
 import VueFusionChartsComponent from "vue-fusioncharts/component";
 import axios from "axios";
-import { USE_LAMBDA } from "../config.js";
 import {
   COMPUTE_WEIGHTS,
   COMPUTE_HISTORICAL_POS,
@@ -113,7 +143,23 @@ export default {
       api_secret: "",
       assets: {},
       computed_assets: {},
+      // Card Data
+      most_profitable: "",
+      least_profitable: "",
+      total_pnl: 0,
+      max_weight: 0,
+      coin_with_max_weight: "",
+      portfolio_value: 0,
+      cards_data: [
+        this.portfolio_value,
+        this.total_pnl,
+        this.most_profitable,
+        this.least_profitable,
+        this.max_weight,
+        this.coin_with_max_weight,
+      ],
       componentKey: 0,
+      // Plotting Data
       pieChartData: {
         chart: {
           caption: "Current Portfolio Distribution",
@@ -190,6 +236,7 @@ export default {
     NavBar,
     Loading,
     BTable,
+    BCard,
   },
   methods: {
     // Convert unix timestamp to date time format !!! Credit: @esd_mentino project
@@ -234,8 +281,11 @@ export default {
     },
     computeWeights() {
       axios.post(COMPUTE_WEIGHTS, { data: this.assets }).then((res) => {
-        (this.pieChartData.data = res.data.plotting_data),
-          (this.computed_assets = res.data.data);
+        this.pieChartData.data = res.data.plotting_data;
+        this.computed_assets = res.data.data;
+        this.max_weight = res.data.max_weight;
+        this.coin_with_max_weight = res.data.coin_with_max_weight;
+        this.portfolio_value = res.data.portfolio_value;
       });
     },
     computePnl() {
@@ -245,64 +295,31 @@ export default {
         order_history: this.order_history,
       };
       axios.post(COMPUTE_PNL, json_payload).then((res) => {
-        this.pnlChartData.categories[0].category = res.data.data.categories;
-        this.pnlChartData.dataset[0].data = res.data.data.real_pnl;
-        this.pnlChartData.dataset[1].data = res.data.data.ureal_pnl;
+        this.pnlChartData.categories[0].category = res.data.categories;
+        this.pnlChartData.dataset[0].data = res.data.real_pnl;
+        this.pnlChartData.dataset[1].data = res.data.ureal_pnl;
+        this.most_profitable = res.data.most_profitable;
+        this.least_profitable = res.data.least_profitable;
+        this.total_pnl = res.data.total_pnl;
         this.loading = false;
       });
     },
     computeHistoricalPosition() {
-      axios
-        .post(COMPUTE_HISTORICAL_POS, {
-          data: this.order_history,
-        })
-        .then((res) => {
-          // Endpoint returns time in unix form, need to convert back for Fusioncharts to parse
-          var clean_data = [];
-          res.data.data.forEach((item) =>
-            clean_data.push([this.timeConverter(item[0]), item[1], item[2]])
-          );
-          const fusionTable = new FusionCharts.DataStore().createDataTable(
-            clean_data,
-            this.timeSeriesSchema
-          );
-          this.timeSeriesData.data = fusionTable;
-        });
-    },
-    LambdaComputeWeights() {
-      axios.post(COMPUTE_WEIGHTS, this.assets).then((res) => {
-        (this.pieChartData.data = res.data.body.plotting_data),
-          (this.computed_assets = res.data.body.data);
-      });
-    },
-    LambdaComputePnl() {
       var json_payload = {
-        API_KEY: this.api_key,
-        API_SECRET: this.api_secret,
-        order_history: this.order_history,
+        data: this.order_history,
       };
-      axios.post(COMPUTE_PNL, json_payload).then((res) => {
-        this.pnlChartData.categories[0].category = res.data.body.data.categories;
-        this.pnlChartData.dataset[0].data = res.data.body.data.real_pnl;
-        this.pnlChartData.dataset[1].data = res.data.body.data.ureal_pnl;
-        this.loading = false;
+      axios.post(COMPUTE_HISTORICAL_POS, json_payload).then((res) => {
+        // Endpoint returns time in unix form, need to convert back for Fusioncharts to parse
+        var clean_data = [];
+        res.data.data.forEach((item) =>
+          clean_data.push([this.timeConverter(item[0]), item[1], item[2]])
+        );
+        const fusionTable = new FusionCharts.DataStore().createDataTable(
+          clean_data,
+          this.timeSeriesSchema
+        );
+        this.timeSeriesData.data = fusionTable;
       });
-    },
-    LambdaComputeHistoricalPosition() {
-      axios
-        .post(COMPUTE_HISTORICAL_POS, this.order_history)
-        .then((res) => {
-          // Endpoint returns time in unix form, need to convert back for Fusioncharts to parse
-          var clean_data = [];
-          res.data.body.data.forEach((item) =>
-            clean_data.push([this.timeConverter(item[0]), item[1], item[2]])
-          );
-          const fusionTable = new FusionCharts.DataStore().createDataTable(
-            clean_data,
-            this.timeSeriesSchema
-          );
-          this.timeSeriesData.data = fusionTable;
-        });
     },
   },
   mounted() {
@@ -311,15 +328,9 @@ export default {
     this.api_key = this.$store.state.api_details.key;
     this.assets = this.$store.state.assets;
     this.order_history = this.$store.state.order_history;
-    if (USE_LAMBDA) {
-      this.computeWeights();
-      this.computeHistoricalPosition();
-      this.computePnl();
-    } else {
-      this.LambdaComputeWeights();
-      this.LambdaComputeHistoricalPosition();
-      this.LambdaComputePnl();
-    }
+    this.computeWeights();
+    this.computeHistoricalPosition();
+    this.computePnl();
   },
 };
 </script>
