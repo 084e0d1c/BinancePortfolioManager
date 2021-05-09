10afb41117b49ef777781f0e9d4014252a78d762
Brandon <brandu.tan@gmail.com>
2021-05-10 00:39:43 +0800

    Included more information in About

--- frontend/bpm/src/components/About.vue
+++ frontend/bpm/src/components/About.vue
@@ -2,27 +2,29 @@
   <div>
     <loading :active.sync="loading"></loading>
     <div class="container-fluid" style="margin-top: 25px">
-      <div class="row justify-content-center">
-        <div class="col-sm-6 jumbotron" id="header" style="background: #42b983">
-          <h1 class="display-5" style="color: black">
-            Bianance Portfolio Manager
-          </h1>
-          <p class="lead" style="color: black">
-            About this website
-          </p>
-        </div>
-      </div>
+      <logo headerText="About this website"></logo>
     </div>
 
-    <div class="container-fluid">
+    <div class="container-fluid mt-2">
       <div class="row justify-content-center" id="logIn">
-        <div class="col-sm-5">
+        <div class="col-sm-6">
+          <h2>Important</h2>
+          <p>
+            In no way or form is this website related to the official binance
+            platform. This project is open sourced - meaning that the code base
+            is transparent and open for public viewing. You can find it
+            <a
+              href="https://github.com/brandontjd/BinancePortfolioManager"
+              style="color: #42b983"
+              >here</a
+            >
+          </p>
           <h2>What data is collected</h2>
           <p>
-            On this site, we do not collect any of your data. The logging in
-            function implemented is merely a way to provide a better user
-            experience, so that you do not have to manually enter your traded
-            pairs each time.
+            On this site, <b>we do not collect any of your data.</b> The unique
+            user id function implemented is merely a way to provide a better
+            user experience, so that you do not have to manually enter your
+            traded pairs each time.
           </p>
           <p>
             Your trade history is constructed using your API credentials. Once
@@ -49,23 +51,27 @@
               Binance is the only exchange that you use to make your trades
             </li>
             <li>
-              Your portfolio holdings are constructed using your historical
-              trades
+              Your portfolio holdings are constructed using your historical spot
+              account trades plus value of USDT / BUSD / DAI in your spot
+              wallet.
             </li>
             <li>
-              Referral, staking and external deposits will not be included in
-              portfolio value until you have executed a trade with that new
-              value
+              Referral, staking and external deposits profits and values will
+              not be included in portfolio value until you have executed a trade
+              with that new said value
             </li>
             <li>
-              Margin trading P&L is not included
+              Margin trading P&L is not included (you can use the native P&L
+              provided by Binance. It works fine)
             </li>
           </ol>
           <br />
-          <h2>How to obtain Binance API Key and Secret</h2>
+          <h2>How to obtain API Key and Secret</h2>
           <p>
             Refer to
-            <a href="https://www.binance.com/en/support/articles/360002502072"
+            <a
+              href="https://www.binance.com/en/support/articles/360002502072"
+              style="color: #42b983"
               >Binance's official support documentation</a
             >
             on how to do it. Ensure that when generating the key
@@ -73,9 +79,8 @@
           </p>
         </div>
       </div>
-      <br />
       <div class="row justify-content-center">
-        <div class="col-sm-2 mt-2 ">
+        <div class="col-sm-2 mb-5 ">
           <div class="text-center">
             <button class="btn btn-dark btn-block" @click.prevent="gotoHome()">
               I understand the limitations
@@ -88,8 +93,12 @@
 </template>
 
 <script>
+import Logo from "./Logo";
 export default {
   name: "About",
+  components: {
+    Logo,
+  },
   methods: {
     gotoHome() {
       this.$router.push("/");
